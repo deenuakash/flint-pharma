e898c44e09b6a3286c6ebf03e59119ba5a36ed14
deenu <akashraja1108@gmail.com>
2023-12-30 01:51:15 +0530

    Completed Header and made it responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import logo from "../../assets/header logo.svg";
 import "./Header.css";
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [showMenu, setMenu] = useState(false);
+
   return (
     <header>
       <div className="logo__container">
@@ -11,13 +14,21 @@ const Header = () => {
         </Link>
       </div>
       <nav>
-        <ul>
+        <div
+          className={showMenu ? "menu open" : "menu"}
+          onClick={() => setMenu((prev) => !prev)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={showMenu ? "open" : ""}>
           <li>
             <NavLink to="/products">Products</NavLink>
-          </li>{" "}
+          </li>
           <li>
             <NavLink to="/aboutUs">About us</NavLink>
-          </li>{" "}
+          </li>
           <li>
             <NavLink to="/contactUs">Contact us</NavLink>
           </li>
